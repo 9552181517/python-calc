4b9edfc2bb04ad39dc331609b2e22e5c0ca98395
Gaurishankar Jatte <gaurishankarjatte@gmail.com>
2025-06-13 14:49:01 +0530

    Add files via upload

---
+++ calculater.py
@@ -0,0 +1,13 @@
+fnumber = int(input("Enter your first number :"))
+snumber = int(input("Enter your second number :"))
+opration = input("Enter any opration like +,-,*,/,%  : ")
+if opration == "+":
+    print(fnumber + snumber)
+elif opration == "-":
+    print(fnumber - snumber)
+elif opration == "*":
+    print(fnumber * snumber)
+elif opration == "/":
+    print(fnumber / snumber)
+elif opration == "%":
+    print(fnumber % snumber) 
